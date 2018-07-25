7dca9cafbaf94cc842531afc3f1742c6f6fefd6c
Huan An <huanan_cheng@mymail.sutd.edu.sg>
2018-07-25 10:07:19 +0700

    added shorter and new ways to format datetime

--- format_datetime.py
+++ format_datetime.py
@@ -12,3 +12,12 @@ s = now.strftime("%d %B %Y, %H:%M:%S")
 # https://docs.python.org/3/library/datetime.html#datetime.datetime
 
 print(s)  # 12 July 2018, 17:25:50
+
+''' OR A SHORTER WAY'''
+import time
+
+# if no timezone argument is specified behind after your string, local time is used
+print(time.strftime('%d %B %Y, %T'))
+
+# else if you want GMT, use time.gmtime()
+print(time.strftime(("%b %d %Y %T"), time.gmtime()))
